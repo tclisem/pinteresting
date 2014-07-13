2c751ee05436646fda831d4cc95d62eb134e09ca
TimClise <cliset@gmail.com>
2014-07-13 00:11:51 -0400

    Add Paperclip gem and ability to uplaod images

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.2.4'
+gem 'paperclip', '~> 4.0'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
+     belongs_to :user
+     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     do_not_validate_attachment_file_type :image
 end
